253f9a231212bebdbd18988ca3cde8e07051c952
Mido <akeelmido@gmail.com>
2022-01-07 10:20:57 -0500

    Add files via upload

---
+++ luigi-tasks.py
@@ -0,0 +1,18 @@
+import luigi
+
+from tasks import Task1, Task2, Task3, Task4
+
+class Pipeline1(luigi.WrapperTask):
+    def requires(self):
+        yield Task1()
+        yield Task2()
+        yield Task3()
+
+class Pipeline2(luigi.WrapperTask):
+    def requires(self):
+        yield Task4()
+
+
+if __name__=='__main__':
+    # luigi.build(Pipeline1, Pipeline2)
+    pass
---
+++ tasks.py
@@ -0,0 +1,71 @@
+import luigi
+from luigi.contrib.s3 import S3Target, S3Client
+import pandas as pd
+
+class Task1(luigi.Task):
+    def requires(self):
+        return None
+
+    def output(self):
+        return luigi.LocalTarget('stuff/task1.csv')
+
+    def run(self):
+        print('task 1 complete...')
+        print()
+        print()
+        print()
+        print()
+        t = pd.DataFrame()
+        with self.output().open('w') as outtie:
+            outtie.write('complete')
+
+class Task2(luigi.Task):
+    def requires(self):
+        return None
+
+    def output(self):
+        return luigi.LocalTarget('stuff/task2.csv')
+
+    def run(self):
+        print('task 2 complete...')
+        t = pd.DataFrame()
+        print()
+        print()
+        print()
+        print()
+        with self.output().open('w') as outtie:
+            outtie.write('complete')
+
+class Task3(luigi.Task):
+    def requires(self):
+        return None
+
+    def output(self):
+        return luigi.LocalTarget('stuff/task3.csv')
+
+    def run(self):
+        print('task 3 complete...')
+        t = pd.DataFrame()
+        print()
+        print()
+        print()
+        print()
+        with self.output().open('w') as outtie:
+            outtie.write('complete')
+
+class Task4(luigi.Task):
+    def requires(self):
+        return None
+
+    def output(self):
+        return luigi.LocalTarget('stuff/task4.csv')
+
+    def run(self):
+        print('task 4 complete...')
+        print()
+        print()
+        print()
+        print()
+        t = pd.DataFrame()
+        with self.output().open('w') as outtie:
+            outtie.write('complete')
