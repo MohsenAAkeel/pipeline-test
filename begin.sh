804c436bf4f42a2a0f8a0455a9095f7d1bb30c15
Mido <akeelmido@gmail.com>
2022-01-10 14:46:17 -0500

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -2,8 +2,8 @@
 
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 
-python -m luigi --module luigi-tasks Pipeline1 --local-scheduler
+luigi --module luigi-tasks Pipeline1 --local-scheduler
 echo $?
 
-python -m luigi --module luigi-tasks Pipeline3 --local-scheduler
+luigi --module luigi-tasks Pipeline3 --local-scheduler
 echo $?
