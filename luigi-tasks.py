88f6ede356a13a8c618e432070040a2bbe95c040
Mido <akeelmido@gmail.com>
2022-01-10 13:15:58 -0500

    Update luigi-tasks.py

--- luigi-tasks.py
+++ luigi-tasks.py
@@ -15,7 +15,15 @@ class Pipeline2(luigi.WrapperTask):
     def requires(self):
         yield Task4()
 
-
+class Pipeline3(luigi.Task):
+    def requires():
+        None
+    def output():
+        return luigi.LocalTarget('stuff/task5.csv')
+    def run():
+        raise ValueError
+        
+        
 if __name__=='__main__':
     # luigi.build(Pipeline1, Pipeline2)
     pass
