a951f4a08261e514735cb0a262ca64704393dd12
Mido <akeelmido@gmail.com>
2022-01-11 09:46:06 -0500

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -2,5 +2,5 @@
 set -e
 export LUIGI_CONFIG_PATH='.'
 export PYTHONPATH='.' 
-luigi --module luigi-tasks Pipeline1 --local-scheduler
+python -m luigi --module luigi-tasks Pipeline1 --local-scheduler
 echo $?
